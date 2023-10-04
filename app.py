f35a4d558839584ddaf013c79793c346366c1571
fahr-khadija <100168693+fahr-khadija@users.noreply.github.com>
2023-10-03 23:59:04 -0400

    add

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def welcome():
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/start_date<br/>"
-        f"/api/v1.0/end_date"
+        f"/api/v1.0/<start_date>/<end_date>"
     )
 
 # Define the /api/v1.0/precipitation route
